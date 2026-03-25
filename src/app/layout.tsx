ded04c44b0db0ba39fa987fd608f40e953937c18
Diego Vieira <diegovenanciovieira@gmail.com>
2026-03-25 13:51:56 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "AMAR",
-  description: "Experiência premium AMAR",
+  description: "Aplicativo da Mulher Aracajuana",
 };
 
 export default function RootLayout({
